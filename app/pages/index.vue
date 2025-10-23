c250fe042111c96dc1c7860981f19d3d2acc226b
Satoshi Tomita <S.Tomita2023@keio.jp>
2025-10-23 11:21:15 +0900

    Initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="min-h-screen grid place-items-center">
+    <h1
+      class="text-5xl md:text-6xl font-extrabold text-red-600 hover:text-emerald-700 transition text-center"
+    >
+      Hello World
+    </h1>
+  </main>
+</template>
